7cf03088525db8a28ad8943dd84583e7be3c1d83
Vladislav Atakhanov <eloh1mZee@gmail.com>
2024-05-01 13:37:46 +0500

    feat: ThemeSwitcher icons

--- src/components/theme-switcher.svelte
+++ src/components/theme-switcher.svelte
@@ -1,25 +1,31 @@
 <script>
+    import Sun from "./icons/sun.svelte"
+    import Moon from "./icons/moon.svelte"
+    import SunAndMoon from "./icons/sun-and-moon.svelte"
     let theme = "light"
 
     const themes = [
         {
             value: "light",
+            icon: Sun,
         },
         {
             value: "system",
+            icon: SunAndMoon,
         },
         {
             value: "dark",
+            icon: Moon,
         },
     ]
 </script>
 
 <fieldset class="theme-switcher">
-    {#each themes as { value }}
+    {#each themes as { value, icon }}
         <label class="item">
             <input type="radio" name="theme" {value} bind:group={theme} />
             <div class="icon">
-                <div class="ICON"></div>
+                <svelte:component this={icon} />
             </div>
         </label>
     {/each}
@@ -33,10 +39,6 @@
         display: flex;
         border: none;
         margin: 0;
-    }
-    .ICON {
-        width: 1em;
-        height: 1em;
     }
     .icon {
         font-size: 19px;
